b800ed77b7353d8e5c2f368b541bc630f01d9463
ThanhHai151 <thanhhai1512005@gmail.com>
2025-04-24 22:51:47 +0700

    sa

--- src/view/colorVariable.py
+++ src/view/colorVariable.py
@@ -13,3 +13,10 @@ Ocean_Blue = "#006494"
 Sky_Harbor = "#247ba0"
 Bright_Azure = "#1b98e0"
 Ice_Mist = "#e8f1f2"
+fg_color = "#2E2E2E"
+
+red_Calendar    = "#DA4239"
+green_Calendar  = "#9FE481"
+yellow_Calendar = "#F6E785"
+
+
